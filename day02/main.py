91a37723868fd6de1b0ec19d8c42b16d9eef5490
Richard Nixon <rnixonaf@gmail.com>
2021-08-03 14:18:05 -0300

    Update main.py

--- day02/main.py
+++ day02/main.py
@@ -10,3 +10,13 @@ kg = float(input("what's your weight?"))
 hgh = float(input("What's your height?"))
 print((kg) / (hgh ** 2))
 
+#####################################
+
+
+age = input("How old are you?")
+age_dif = 90 - int(age)
+age_final_days = 365 * age_dif
+weeks = 52 * age_dif
+moth = 12 * age_dif
+
+print(f"{age_final_days} Dias, {weeks} Semanas e {moth} Meses left" )
